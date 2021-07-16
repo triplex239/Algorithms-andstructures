79939f2ee539431ffc2f76a7871cb01f4136756f
Илья <@DESKTOP-763PSIT>
2021-07-16 18:38:43 +0300

    Добавьте файлы проекта.

---
+++ Les5algor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Les5algor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ newList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace newList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int someAnotherValue = 42;
+            {
+                int scopedValue = 5;
+                Console.WriteLine($"{someAnotherValue} {scopedValue}");
+            }
+            {
+                int scopedValue = 2;
+                Console.WriteLine($"{someAnotherValue} {scopedValue}");
+            }
+
+            Console.WriteLine($"{someAnotherValue}");
+            Console.WriteLine($"{scopedValue}");
+
+        }
+    }
+}
